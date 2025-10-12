9da1a741bb2d3b698ae5a175dbbf93252944976c
Amine-Azhari <amineelazhari326@gmail.com>
2025-10-12 15:26:23 +0200

    primeNums.c

--- TD1/EX2/primeNums.c
+++ TD1/EX2/primeNums.c
@@ -53,16 +53,3 @@ int dyn_primeNumbers(int n)
 }
 
 
-
-
-/*
-void main()
-{
-    int n = 20;
-    int primes = primeNumbers(n);
-    int dyn_primes = dyn_primeNumbers(n);
-    printf("primes= %d\n", primes);
-    printf("dyn_primes= %d\n", dyn_primes);
-
-}
-*/
