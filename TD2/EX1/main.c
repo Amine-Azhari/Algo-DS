7faac9d019e53c375316e667236874932d770b9e
Amine-Azhari <amineelazhari326@gmail.com>
2025-10-20 10:13:41 +0200

    Update main.c

--- TD2/EX1/main.c
+++ TD2/EX1/main.c
@@ -7,7 +7,7 @@
 int main(void)
 {
 
-    int arr[100];
+    int* arr = malloc(50 * sizeof(int));
     for (int i=0; i<50; i++)
     {
         arr[i] = i*12;
@@ -15,17 +15,26 @@ int main(void)
     int target = 144;
     if (is_sorted_nondecreasing(arr,50))
     {
-        int jump_s = jump_search(arr, 100, target);
+        clock_t start_j = clock();
+        int jump_s = jump_search(arr, 50, target);
+        clock_t end_j = clock();
+
+        clock_t start_b = clock();
         int bin_s = binary_search(arr, 50, target);
-        printf("jump : %d\n", jump_s);
-        printf("binary: %d\n", bin_s);
+        clock_t end_b = clock();
+
+        printf("jump : %d , done in : %d ms\n", jump_s, 1000*(start_j-end_j));
+        printf("binary: %d , done in : %d ms\n", bin_s, 1000*(start_b-end_b));
 
     }
     else
     {
+        clock_t start_l = clock();
         int lin_s = linear_search(arr, 50, target);
-        printf("linear: %d\n", lin_s);
+        clock_t end_l = clock();
+        printf("linear: %d , done in : %d ms\n", lin_s, 1000*(start_l-end_l));
     }
+
 
     return 0;
 }
